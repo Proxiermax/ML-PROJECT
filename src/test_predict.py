96b5adfbe54d67378401454f170f32e616113a49
billza555 <nphisu@gmail.com>
2026-02-22 19:05:57 +0700

    feat: create multiple regression

--- src/test_predict.py
+++ src/test_predict.py
@@ -1,8 +1,10 @@
 from src.modeling.linear_regression.predict import predict as linear_regression_predict
 from src.modeling.poly_regression.predict import predict as poly_regression_predict
+from src.modeling.multiple_regression.predict import predict as multiple_regression_predict
 
 if __name__ == "__main__":
     raw_input = input("Enter features separated by comma: ")
     values = list(map(float, raw_input.split(",")))
     print("Prediction:", linear_regression_predict(values))
     print("Prediction:", poly_regression_predict(values))
+    print("Prediction:", multiple_regression_predict(values))
